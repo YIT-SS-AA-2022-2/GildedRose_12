e997b795ad49bee8a7dc4c9246d44c9b13cec4fb
김아정 <92311286+rladkwjd@users.noreply.github.com>
2022-12-03 16:54:08 +0900

    Add files via upload

---
+++ test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GildedRoseTest {
+    @Test
+    public void foo() {
+        GildedRose app = testItem("test", 0, 0);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    private GildedRose testItem(String item, Integer sellin, Integer quality) {
+        Item[] items = new Item[]{new Item(item, sellin, quality)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        return app;
+    }
+}
